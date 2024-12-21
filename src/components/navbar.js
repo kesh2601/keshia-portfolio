6a43315af4b76030cd26f19683e2d8cc90afd521
Keshia Adrin <workwithkeshia@gmail.com>
2024-12-20 20:03:39 -0800

    navbar collapse

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -6,21 +6,21 @@ function Navbar() {
 				<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent">
 					<span class="navbar-toggler-icon"></span>
 				</button>
-				<div className="collapse navbar-collapse">
-				<ul class="navbar-nav mr-auto">
-					<li className="nav-item">
-						<a class="nav-link" href="/">Home <span class="sr-only"></span></a>
-					</li>
-					<li className="nav-item">
-						<a class="nav-link" href="works">Works <span class="sr-only"></span></a>
-					</li>	
-					<li className="nav-item">
-						<a class="nav-link" href="resume">Resume <span class="sr-only"></span></a>
-					</li>			
-				</ul>
+
+				<div className="collapse navbar-collapse" id="navbarSupportedContent">
+					<ul class="navbar-nav mr-auto">
+						<li className="nav-item active">
+							<a class="nav-link" href="/">Home <span class="sr-only"></span></a>
+						</li>
+						<li className="nav-item">
+							<a class="nav-link" href="works">Works</a>
+						</li>
+						<li className="nav-item">
+							<a class="nav-link" href="resume">Resume</a>
+						</li>
+					</ul>
 				</div>
 			</div>
-
 		</>
 	)
 }
