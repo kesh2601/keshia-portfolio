8907424b5f21dcfa2064f4633dd3a101be5824fb
Keshia Adrin <workwithkeshia@gmail.com>
2024-12-20 19:44:22 -0800

    navbar initial

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-import './App.css';
 import Navbar from './components/navbar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
         <Navbar> </Navbar>
-      </header>
     </div>
   );
 }
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,8 +2,23 @@ import React from "react";
 function Navbar() {
 	return (
 		<>
-			<div class="bg-primary">
-				Hello Keshia
+			<div className="navbar navbar-expand-sm navbar-dark bg-success">Hello Keshia
+				<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<div className="collapse navbar-collapse">
+				<ul class="navbar-nav mr-auto">
+					<li className="nav-item">
+						<a class="nav-link" href="/">Home <span class="sr-only"></span></a>
+					</li>
+					<li className="nav-item">
+						<a class="nav-link" href="works">Works <span class="sr-only"></span></a>
+					</li>	
+					<li className="nav-item">
+						<a class="nav-link" href="resume">Resume <span class="sr-only"></span></a>
+					</li>			
+				</ul>
+				</div>
 			</div>
 
 		</>
